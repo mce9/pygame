9077d63024af3e92e5d57c75c8b93a0e59c3cdc7
mce9 <dwing@noncosi.com>
2020-03-14 15:08:35 +0100

    Add messagequeue.py

---
+++ messagequeue.py
@@ -0,0 +1,17 @@
+from pygame.locals import *
+from sys import exit
+import pygame
+
+pygame.init()
+SCREEN_SIZE = (800, 600)
+screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+        print(event)
+    
+    pygame.display.update()
