1880f2962f6de6db3e2a1a4ed4013408db8f2dbb
mce9 <dwing@noncosi.com>
2020-04-04 22:17:17 +0200

    Update antstatemachine.py

--- antstatemachine.py
+++ antstatemachine.py
@@ -192,3 +192,55 @@ class Ant(GameEntity):
 			w, h = self.carry_image.get_size()
 			surface.blit(self.carry_image, (x-w, y-h/2))
 
+class AntStateExploring(State):
+
+	def __init__(self, any):
+		State.__init__(self, "exploring")
+		self.ant = ant
+
+	def random_destination(self):
+		w, h = SCREEN_SIZE
+		self.ant.destination = Vector2(randint(0, w), randint(0, h))
+
+	def do_actions(self):
+		if randint(1, 20) == 1:
+			self.random_destination()
+
+	def check_conditions(self):
+		leaf = self.ant.world.get_close_entity("leaf", self.ant.location)
+		if leaf is not None:
+			self.ant.leaf_id = leaf.id
+			return "seeking"
+
+		spider = self.ant.world.get_close_entity("spider", NEST_POSITION, NEST_SIZE)
+		if spider is not None:
+			if self.ant.location.get_distance:to(spider.location) < 100:
+				self.ant.spider_id = spider.id
+				return "hunting"
+
+		return None
+
+		def entry_actions(self):
+			self.ant.speed = 120 + randint(-30, 30)
+			self.random_destination()
+
+class AntStateSeeking(State):
+
+	def __init__(self, ant):
+		State.__init__(self, "seeking")
+		self.ant = ant
+		self.leaf_id = None
+
+	def check_conditions(self):
+		leaf = self.ant.world.get(self.ant.leaf_id)
+		if leaf is None:
+			return "exploring"
+
+		if self.ant.location.get_distance:to(leaf.location) < 5:
+			self.ant.carry(leaf.image)
+			self.ant.world.remove_entity(leaf)
+			return "delivering"
+
+		return None
+
+	def 
