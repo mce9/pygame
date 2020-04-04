d0f8a08c1993a03342cad5d91353490699a6b70a
mce9 <dwing@noncosi.com>
2020-04-04 21:44:12 +0200

    Update antstatemachine.py

--- antstatemachine.py
+++ antstatemachine.py
@@ -163,4 +163,32 @@ class Ant(GameEntity):
 	def __init__(self, world, image):
 		GameEntity.__init__(self, world, "ant", image)
 
-		
+		exploring_state = AntStateExploring(self)
+		seeking_state = AntStateExploring(self)
+		delivering_state = AntStateExploring(self)
+		hunting_state = AntStateExploring(self)
+
+		self.brain.add_state(exploring_state)
+		self.brain.add_state(seeking_state)
+		self.brain.add_state(delivering_state)
+		self.brain.add_state(hunting_state)
+
+		self.carry_image = None
+
+	def carry(self, image):
+		self.carry_image = image
+
+	def drop(self, surface):
+		if self.carry_image:
+			x, y = self.location
+			w, h = self.carry_image.get_size()
+			surface.blit(self.carry_image, (x-w, y-h/2))
+			self.carry_image = None
+
+	def render(self, surface):
+		GameEntity.render(self, surface)
+		if self.carry_image:
+			x, y = self.location
+			w, h = self.carry_image.get_size()
+			surface.blit(self.carry_image, (x-w, y-h/2))
+
